5575431031d76e248a541bc4d9de6c9877250fb7
muhid <muhid@ihealthscreen.org>
2022-07-03 21:30:10 +0600

    initialize laravel portfolio

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+      <!-- port folio -->
+
+      <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{asset('css/animate.css')}}">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{asset('css/icomoon.css')}}">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="{{asset('css/flexslider.css')}}">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+	<link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+	<!-- Modernizr JS -->
+	<script src="{{asset('js/modernizr-2.6.2.min.js')}}"></script>
+
+      <!-- port folio -->
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+
+        <!-- Bootstrap core JavaScript-->
+        <script src="{{asset('js/app.js')}}"></script>
+
+
+<!-- Custom scripts for all pages-->
+<script src="{{asset('admin/js/sb-admin-2.min.js')}}"></script>
+<script src="{{asset('js/jquery.min.js')}} "></script>
+<!-- jQuery Easing -->
+<script src="{{asset('js/jquery.easing.1.3.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<!-- Waypoints -->
+<script src="{{asset('js/jquery.waypoints.min.js')}}"></script>
+<!-- Flexslider -->
+<script src="{{asset('js/jquery.flexslider-min.js')}}"></script>
+<!-- Owl carousel -->
+<script src="{{asset('js/owl.carousel.min.js')}}"></script>
+<!-- Counters -->
+<script src="{{asset('js/jquery.countTo.js')}}"></script>
+
+
+<!-- MAIN JS -->
+<script src="{{asset('js/main.js')}}"></script>
+
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                
+                <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button> -->
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        <li class="nav-item" style="padding-right: 50px;">
+                            <a class="nav-link" href="{{ url('/') }}">
+                                {{ config('app.name', 'Laravel') }}
+                            </a>
+                        </li>
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item" style="padding-right: 50px;">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item" style="padding-right: 50px;">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+                            </li>
+                            <li class="nav-item" style="padding-right: 50px;">
+                                    <a  href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
